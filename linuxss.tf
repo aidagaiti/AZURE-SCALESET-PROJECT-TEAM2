7dd07b4e1ddf9eaa9d2093beb4c79237b91d1a5e
abdullaherol07 <abdullaherol07@gmail.com>
2022-02-04 13:25:34 -0500

    modify

--- linuxss.tf
+++ linuxss.tf
@@ -1,5 +1,3 @@
-
-
 resource "azurerm_resource_group" "project1" {
   name     = "project1-resources"
   location = "West US"
@@ -52,6 +50,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "linux1" {
       name      = "subnet2"
       primary   = true
       subnet_id = azurerm_subnet.subnet2.id
+       private_ip_address_allocation = "Dynamic"
     }
   }
 }
